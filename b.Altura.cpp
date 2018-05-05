65cdf83f41bc70d9c216c8d2e2b59450e380c933
joseferqm <jfqm26@gmail.com>
2018-05-05 16:10:53 -0600

    completa algoritmo ejercicio b

--- b.Altura.cpp
+++ b.Altura.cpp
@@ -1,3 +1,25 @@
 Altura(nRef, A) {
-    
+    C.Iniciar()
+    C.Encolar(nRef)
+    altura = 0
+    contNivActual = 1
+    contNivSiguiente = 0
+    while !C.Vacia() {
+        if (contNivActual == 0) {
+            altura++
+            contNivActual = contNivSiguiente
+            contNivSiguiente = 0
+        }
+
+        n = C.Desencolar()
+        contNivActual--
+        nh = A.HijoMasIzquierdo(n)
+        while nh != nodo_nulo {
+            C.Encolar(nh)
+            contNivSiguiente++
+            nh = A.HermanoDerecho(nh)
+        }
+    }
+    C.Destruir()
+    return altura
 }
