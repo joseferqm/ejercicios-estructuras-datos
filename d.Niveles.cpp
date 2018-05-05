b9fec1e6f798a16dbe76abd4243f752dfc9a9843
joseferqm <jfqm26@gmail.com>
2018-05-05 17:09:53 -0600

    completa algoritmo ejercicio d

--- d.Niveles.cpp
+++ d.Niveles.cpp
@@ -1,3 +1,27 @@
 Niveles(A) {
-    
+    niveles = 0
+    if !A.Vacio() {
+        C.Iniciar()
+        C.Encolar(A.Raiz())
+        niveles = 1
+        contNodosNivelActual = 1
+        contNodosNivelSiguiente = 0
+        while !C.Vacia() {
+            if contNodosNivelActual == 0 {
+                niveles++
+                contNodosNivelActual = contNodosNivelSiguiente
+                contNodosNivelSiguiente = 0
+            }
+
+            n = C.Desencolar()
+            contNodosNivelActual--
+            nh = A.HijoMasIzquierdo(n)
+            while nh != nodo_nulo {
+                C.Encolar(nh)
+                contNodosNivelSiguiente++
+                nh = A.HermanoDerecho(nh)
+            }
+        }
+    }
+    return niveles
 }
