77abe05e60b43a24120ba20b2267c4ab89f07447
joseferqm <jfqm26@gmail.com>
2018-05-05 12:29:26 -0600

    completa algoritmo ejercicio a

--- a.HermanoIzquierdo.cpp
+++ a.HermanoIzquierdo.cpp
@@ -1,3 +1,29 @@
 HermanoIzquierdo(nRef, A) {
-    
+    C.Iniciar()
+    C.Encolar(A.Raiz())
+    n = nodo_nulo
+    while !C.Vacia() {
+        n = C.Desencolar()
+        nh = A.HijoMasIzquierdo(n)
+        if nh == nRef {
+            n = nodo_nulo
+            C.Vaciar()
+        }
+        else {
+            while nh != nodo_nulo {
+                n = A.HermanoDerecho(nh)
+                if n == nRef {
+                    n = nh
+                    nh = nodo_nulo
+                    C.Vaciar()
+                }
+                else {
+                    C.Encolar(nh)
+                    nh = n
+                }
+            }
+        }
+    }
+    C.Destruir()
+    return n
 }
