1c10e4ed1cc5d16118960041079dfee619fba362
joseferqm <jfqm26@gmail.com>
2018-05-06 08:45:04 -0600

    completa algoritmo ejercicio d recurs

--- d.NivelesRecurs.cpp
+++ d.NivelesRecurs.cpp
@@ -1,7 +1,21 @@
 Niveles(A) {
-
+    if A.Vacio() {
+        return 0
+    }
+    else {
+        NivelesR(A.Raiz())
+    }
 }
 
 NivelesR(n) {
-    
+    niveles = 0
+    nh = A.HijoMasIzquierdo(n)
+    while nh != nodo_nulo {
+        nivelesnh = NivelesR(nh)
+        if nivelesnh > niveles {
+            niveles = nivelesnh
+        }
+        nh = A.HermanoDerecho(nh)
+    }
+    return niveles + 1
 }
