e41afe8f320722c80280d0212a03fac43bd1f406
joseferqm <jfqm26@gmail.com>
2018-05-06 11:29:29 -0600

    completa algoritmo ejercicio p

--- p.NumNodosNivel.cpp
+++ p.NumNodosNivel.cpp
@@ -1,3 +1,26 @@
 NumNodosNivel(i, A) {
-    
+    numNodos = 0
+    C.Iniciar()
+    CNiveles.Iniciar()
+    C.Encolar(A.Raiz())
+    CNiveles.Encolar(1)
+    while !C.Vacia() {
+        n = C.Desencolar()
+        niveln = CNiveles.Desencolar()
+        if niveln == i {
+            numNodos++
+        }
+        else {
+            nh = A.HijoMasIzquierdo(n)
+            nivelnh = niveln + 1
+            while nh != nodo_nulo {
+                C.Encolar(nh)
+                CNiveles.Encolar(nivelnh)
+                nh = A.HermanoDerecho(nh)
+            }
+        }
+    }
+    C.Destruir()
+    CNiveles.Destruir()
+    return numNodos
 }
