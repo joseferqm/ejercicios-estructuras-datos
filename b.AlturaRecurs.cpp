e5aec0adeb510d2a96e00af7ececedab4da4a051
joseferqm <jfqm26@gmail.com>
2018-05-06 08:30:18 -0600

    completa algoritmo ejercicio b recurs

--- b.AlturaRecurs.cpp
+++ b.AlturaRecurs.cpp
@@ -1,7 +1,34 @@
 Altura(n, A) {
-
+    return AlturaR(n)
 }
 
-AlturaR(n) {
+AlturaR(n) {    // OPCIÓN 1
+    if A.EsHoja() {
+        return 0
+    }
+    else {
+        altura = 0
+        nh = A.HijoMasIzquierdo(n)
+        while nh != nodo_nulo {
+            alturanh = AlturaR(nh)
+            if alturanh > altura {
+                altura = alturanh
+            }
+            nh = A.HermanDerecho(nh)
+        }
+        return altura + 1
+    }
+}
 
+AlturaR(n) {    // OPCIÓN 2
+    altura = -1
+    nh = A.HijoMasIzquierdo(n)
+    while nh != nodo_nulo {
+        alturan = AlturaR(nh)
+        if alturanh > altura {
+            altura = alturanh
+        }
+        nh = A.HermanoDerecho(nh)
+    }
+    return altura + 1
 }
