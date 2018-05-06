9e83bcf62bc0f37023bd198410f8023cbd571c50
joseferqm <jfqm26@gmail.com>
2018-05-06 10:19:28 -0600

    completa algoritmo ejercicio o

--- o.NumNodosNivel.cpp
+++ o.NumNodosNivel.cpp
@@ -1,7 +1,18 @@
 NumNodosNivel(i, A) {
-
+    return NumNodosNivelR(0, A.Raiz())
 }
 
-NumNodosNivelR(nivelActual, n) {
-
+NumNodosNivelR(nivelPadre, n) {
+    nivelActual = nivelPadre + 1
+    if nivelActual == i {
+        return 1
+    } else {
+        numNodos = 0
+        nh = A.HijoMasIzquierdo(n)
+        while nh != nodo_nulo {
+            numNodos += NumNodoNivelR(nivelActual, nh)
+            nh = A.HermanoDerecho(nh)
+        }
+        return numNodos
+    }
 }
