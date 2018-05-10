6143e1e5c55aaa3826a9b485ffe5690f4025d657
joseferqm <jfqm26@gmail.com>
2018-05-10 12:33:58 -0600

    cambia a variable global para contador

--- o.NumNodosNivel.cpp
+++ o.NumNodosNivel.cpp
@@ -1,18 +1,18 @@
 NumNodosNivel(i, A) {
-    return NumNodosNivelR(0, A.Raiz())
+    contNodosGlobal =0
+    NumNodosNivelR(0, A.Raiz())
+    return contNodosGlobal
 }
 
 NumNodosNivelR(nivelPadre, n) {
     nivelActual = nivelPadre + 1
     if nivelActual == i {
-        return 1
+        contNodosGlobal++
     } else {
-        numNodos = 0
         nh = A.HijoMasIzquierdo(n)
         while nh != nodo_nulo {
-            numNodos += NumNodoNivelR(nivelActual, nh)
+            NumNodoNivelR(nivelActual, nh)
             nh = A.HermanoDerecho(nh)
         }
-        return numNodos
     }
 }
